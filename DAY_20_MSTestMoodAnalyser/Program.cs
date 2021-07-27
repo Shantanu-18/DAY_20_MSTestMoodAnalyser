a1562732adae971caf7fc790f391718db0152690
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-27 19:32:09 +0530

    Add project files.

---
+++ DAY_20_MSTestMoodAnalyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DAY_20_MSTestMoodAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
