b183a23dd5b5cb34171213b25db3723408c0e5fa
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-07-27 20:15:41 +0530

    [Shantanu] Added : Analyse and response Happy or Sad Mood.

---
+++ DAY_20_MSTestMoodAnalyser/MoodAnalyser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSTestMoodAnalyser
+{
+    public class MoodAnalyser
+    {
+        private string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public MoodAnalyser()
+        {
+        }
+        public string analyseMood()
+        {
+            if (this.message.Contains("SAD"))
+                return "SAD";
+            else
+                return "HAPPY";
+        }
+    }
+}
